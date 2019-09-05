7a257d6d323ef1ed53c628feaadd2c78a0b9252c
Laurent Chaverra <ldmckkb@gmail.com>
2019-09-05 08:20:12 -0500

    V3.0
    ya sirve para entradas idealmente perfectas

--- Calculadora.py
+++ Calculadora.py
@@ -1,40 +1,31 @@
 
 def isfloat(string):
-    array = string.split('.')
-    i= 0
+    array = string.split('.')  
+    i = 0
     isnumber = True
-    while not(isnumber) or i<len(array):
-        if not(array[i].isnumeric()):
-            isnumer = False
+    while isnumber and i < len(array):        
+        if not array[i].isnumeric():
+            isnumber = False
         i = i+1
     return isnumber
         
-
 class Arbol:
     root = None
     left = None
-    right = None
-
-    def __init__(self):
-        pass
+    right = None    
     
-    def __init__(self, root, left = None, right = None):
+    def __init__(self, root = None, left = None, right = None):
         self.root = root
         self.left = left
         self.right = right
-
-    def __init__(self):
-        pass
         
-    def addToTree(self, symbol):
-        print(symbol)
+    def addToTree(self, symbol):        
         if isfloat(symbol):
             if self.left == None:
-                self.left = symbol
+                self.left = symbol                
             elif self.right == None:
-                self.right = symbol
-            else:
-                self.right = Arbol()
+                self.right = symbol                
+            else:                
                 self.right.addToTree(symbol)
         elif symbol in ('+','-','*','/','%'):
             if (self.root == None):
@@ -42,44 +33,40 @@ class Arbol:
             elif (self.right == None):
                 self.right = Arbol(symbol)
             else:
-                self.right.addToTree(symbol)
-            
+                self.right.addToTree(symbol)            
         else:
              print('no es posible computar el input')
 
-def getInput(tree,string):
+def processInput(tree, string):
     array = string.split()
     array.reverse()
     for i in range(len(array)):
         tree.addToTree(array[i])
 
 def recorrer(tree):
-    if(type(tree.right) == 'Arbol'):
-        if(tree.right.root == '+'):
-            return tree.left + recorrer(tree.right)
-        elif(tree.right.root == '-'):
-            return tree.left - recorrer(tree.right)
-        elif(tree.right.root == '*'):
-            return tree.left * recorrer(tree.right)
-        elif(tree.right.root == '/'):
-            return tree.left / recorrer(tree.right)
-        elif(tree.right.root == '%'):
-            return tree.left % recorrer(tree.right)
-    else:       
-        fRight = float(tree.right)
-        fLeft = float(tree.left)
+    if type(tree.right) is Arbol:         
         if tree.root == '+':
-            return fRight + fLeft
-        elif tree.root == '-':
-            return fRight - fLeft
+            return float(tree.left) + recorrer(tree.right)
+        elif tree.root == '-':            
+            return float(tree.left) - recorrer(tree.right)
         elif tree.root == '*':
-            return fRight * fLeft
+            return float(tree.left)  * recorrer(tree.right)
         elif tree.root == '/':
-            return fRight / fLeft
+            return float(tree.left) / recorrer(tree.right)
         elif tree.root == '%':
-            return fRight % fLeft    
-        
-cadena = input()
+            return float(tree.left) % recorrer(tree.right)
+    else:       
+        if tree.root == '+':
+            return float(tree.left) + float(tree.right)
+        elif tree.root == '-':
+            return float(tree.left) - float(tree.right)
+        elif tree.root == '*':
+            return float(tree.left) * float(tree.right)
+        elif tree.root == '/':
+            return float(tree.left) / float(tree.right)
+        elif tree.root == '%':
+            return float(tree.left) % float(tree.right)        
+
 arbol = Arbol()
-getInput(arbol, cadena)
-recorrer(arbol)
+processInput(arbol, input())
+print(recorrer(arbol))
